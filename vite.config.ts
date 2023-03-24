d9488bc7d1cc387bc4dc93a3c54e23075d31059a
Brian Ingles <brianingles@deephaven.io>
2023-03-24 08:48:57 -0500

    Ran Prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,28 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-const proxyTarget = "http://localhost:10000";
+const proxyTarget = 'http://localhost:10000'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
     proxy: {
-      "/jsapi": {
+      '/jsapi': {
         target: proxyTarget,
         changeOrigin: true,
       },
-      "^/arrow\\.*": {
+      '^/arrow\\.*': {
         target: proxyTarget,
         changeOrigin: true,
         ws: true,
       },
     },
   },
-});
+})
